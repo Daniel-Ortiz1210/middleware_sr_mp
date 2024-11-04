fde941f23cef30c6cb6e5de38d9ebb13b39debae
Daniel Ortiz <thisisdanielortiz@gmail.com>
2024-11-04 11:14:26 -0600

    add routes/users.js file with endpoints assigned with its controller

---
+++ routes/users.js
@@ -0,0 +1,331 @@
+const express = require('express');
+const { userDetailController, createUserController, deleteUserController, updateUserController } = require('../controllers/users.js');
+const { verifyTokenMiddleware } = require('../token.js');
+
+const router = express.Router();
+
+/**
+ * @openapi
+ * /users/{id}:
+ *   get:
+ *     summary: Obtiene un usuario por ID
+ *     description: Devuelve los detalles del usuario especificado por su ID.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID del usuario a obtener
+ *         schema:
+ *           type: string
+ *           example: "1"
+ *     responses:
+ *       200:
+ *         description: Usuario encontrado con éxito
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   type: object
+ *                   example: { id: 1, username: "usuario1", email: "usuario1@example.com", "password": "contraseñaSegura123!", createdAt: "2024-11-04 11:21:18.552 +0000", updatedAt: "2024-11-04 11:21:18.552 +0000"}
+ *       401:
+ *         description: No autorizado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "No tienes permiso para acceder a este recurso."
+ *       404:
+ *         description: Usuario no encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Usuario no encontrado."
+ *       500:
+ *         description: Error interno del servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Ocurrió un error inesperado."
+ */
+router.get('/:id', verifyTokenMiddleware, userDetailController);
+/**
+ * @openapi
+ * /users/:
+ *   post:
+ *     summary: Crea un nuevo usuario
+ *     description: Crea un nuevo usuario y devuelve un token junto con el usuario creado.
+ *     responses:
+ *       201:
+ *         description: Usuario creado con éxito
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 token:
+ *                   type: string
+ *                   example: "abcd1234efgh5678"
+ *                 data:
+ *                   type: object
+ *                   example: { id: 1, username: "usuario1", email: "usuario1@example.com", "password": "contraseñaSegura123!", createdAt: "2024-11-04 11:21:18.552 +0000", updatedAt: "2024-11-04 11:21:18.552 +0000"}
+ *       400:
+ *         description: Error en el cuerpo de la peticion
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Error en la peticion."
+ *       500:
+ *         description: Error interno del servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Ocurrió un error inesperado."
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "correo@correo.com"
+ *               password:
+ *                 type: string
+ *                 example: "contraseñaSegura123!"
+ */
+router.post('/', createUserController);
+
+/**
+ * @openapi
+ * /users/{id}:
+ *   patch:
+ *     summary: Actualiza un usuario por ID
+ *     description: Actualiza la información del usuario especificado por su ID.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID del usuario a actualizar
+ *         schema:
+ *           type: string
+ *           example: "1"
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: "nuevo_usuario"
+ *               email:
+ *                 type: string
+ *                 example: "nuevo_usuario@example.com"
+ *     responses:
+ *       200:
+ *         description: Usuario actualizado con éxito
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   type: object
+ *                   example: { id: 1, username: "nuevo_usuario", email: "nuevo_usuario@example.com" }
+ *       401:
+ *         description: No autorizado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "No tienes permiso para realizar esta acción."
+ *       404:
+ *         description: Usuario no encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Usuario no encontrado."
+ *       500:
+ *         description: Error interno del servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Ocurrió un error inesperado."
+ */
+router.patch('/:id', verifyTokenMiddleware, updateUserController);
+
+/**
+ * @openapi
+ * /users/{id}:
+ *   delete:
+ *     summary: Elimina un usuario por ID
+ *     description: Elimina el usuario especificado por su ID.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID del usuario a eliminar
+ *         schema:
+ *           type: string
+ *           example: "1"
+ *     responses:
+ *       200:
+ *         description: Usuario eliminado con éxito
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   type: object
+ *                   example: { message: "Usuario eliminado exitosamente." }
+ *       401:
+ *         description: No autorizado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "No tienes permiso para realizar esta acción."
+ *       404:
+ *         description: Usuario no encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Usuario no encontrado."
+ *       500:
+ *         description: Error interno del servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Ocurrió un error inesperado."
+ */
+router.delete('/:id', verifyTokenMiddleware, deleteUserController);
+
+
+module.exports = router;
